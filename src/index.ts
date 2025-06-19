1b41c2f9d9fa390f9868fd7b8378be3a923354ae
anshuman008 <code.anshu@gmail.com>
2025-06-20 04:28:02 +0530

    feat: anchor provider and program added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+import pumpIdl from "./IDL/pump.json";
+import { Pump } from "./IDL/pump";
+import * as anchor from "@coral-xyz/anchor";
+
+import { clusterApiUrl, Connection, Keypair } from "@solana/web3.js/lib";
+
+
+
+const connection = new Connection(clusterApiUrl("mainnet-beta"),"confirmed");
+
+const keypair = Keypair.generate();
+const wallet = new anchor.Wallet(keypair);
+const provider = new anchor.AnchorProvider(connection,wallet);
+
+const pumpProgram = new anchor.Program<Pump>(pumpIdl as Pump,  provider);
+
+
